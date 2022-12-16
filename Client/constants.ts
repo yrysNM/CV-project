620159193c1389174c511fbe2c9b234ae40cd1e9
yrysNM <yrys.n3m@gmail.com>
2022-12-17 01:32:12 +0600

    new project

--- Client/constants.ts
+++
@@ -1,9 +0,0 @@
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-export { prefix };
-
-const CONFIGS = {
-  keypointClassifierLabels: ['Open', 'Closed', 'Pointing'],
-};
-
-export default CONFIGS;
--- Client/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HandsCapture from './hands-capture/index';
-
-function HomePage() {
-  return <HandsCapture />;
-}
-
-export default HomePage;
